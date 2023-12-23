eba317c105cd96d10e5595a68c4badec7ec532f4
eeennsu <diso592@naver.com>
2023-12-23 23:54:54 +0900

    Dropbox 스타일링 적용중

--- src/components/feature/root/main/SearchBar.tsx
+++ src/components/feature/root/main/SearchBar.tsx
@@ -16,16 +16,16 @@ const SearchBar: FC = () => {
 
     return (
         <form className='relative flex items-center lg:gap-4 lg:justify-start w-full max-w-2xl px-2 py-0.5 sm:px-4 sm:py-1 text-black rounded-lg sm:rounded-full max-sm:gap-2.5 max-sm:flex-col bg-slate-300' onSubmit={handleSubmit}>
-            <div className='flex flex-1 w-full'>
+            <div className='flex items-center justify-start flex-1 w-full'>
                 <SearchManufacturer manufacturer={manufacturer} setManufacturer={setManufacturer} />
                 <button className='flex items-center justify-center cursor-pointer bg-inherit sm:hidden' type='submit'>
                     <TbSearch className='transition scale-125 bg-inherit hover:scale-150' />
                 </button>
             </div>
-            <div className='flex flex-1 w-full'>
+            <div className='flex items-center justify-start flex-1 w-full'>
                 <Input className='w-full rounded-full py-0.5 max-sm:mb-2' />
                 <button className='flex items-center justify-center cursor-pointer bg-inherit' type='submit'>
-                    <TbSearch className='transition scale-125 bg-inherit hover:scale-150' />
+                    <TbSearch className='transition scale-125 bg-inherit hover:scale-150 bg-slate-300' />
                 </button>
             </div>
         </form>
--- src/components/feature/root/main/SearchManufacturer.tsx
+++ src/components/feature/root/main/SearchManufacturer.tsx
@@ -1,5 +1,9 @@
-import type { Dispatch, FC, SetStateAction } from 'react';
-import { Input } from '@mui/material';
+import type { Dispatch, FC, ChangeEvent, SetStateAction } from 'react';
+import { useState, forwardRef, Fragment } from 'react';
+import { TbBrandFlickr } from "react-icons/tb";
+import { Input as MuiInput } from '@mui/material';
+import { Combobox, Transition } from '@headlessui/react';
+import { manufacturers } from '@/constants';
 
 type Props = {
     manufacturer: string;
@@ -8,13 +12,86 @@ type Props = {
 
 const SearchManufacturer: FC<Props> = ({ manufacturer, setManufacturer }) => {
 
+    const [query, setQuery] = useState<string>('');
+    const handleManufactuerer = (value: string) => {
+        setManufacturer(value);
+    }
+
+    const handleQuery = (e: ChangeEvent<HTMLInputElement>) => { 
+        setQuery(e.target.value);
+    }
+
+    const initQuery = () => {
+        setQuery('');
+    }
+
+    const filteredManufactures = query === '' ? manufacturers : manufacturers.filter((item) => (
+        item.toLowerCase().replaceAll(' ', '').includes(query)
+    ))
 
     return (
         <>
-            <Input className='w-full rounded-full py-0.5' /> 
+            {/* <Input className='w-full rounded-full py-0.5' />  */}
+            <Combobox value={manufacturer} onChange={handleManufactuerer}>
+                <div className='flex items-center justify-center mr-3'>
+                    <Combobox.Button className='bg-inherit'>
+                        <TbBrandFlickr className='flex items-center justify-center h-6 w bg-inherit bg-slate-300' />
+                    </Combobox.Button>
+                </div>          
+                <Combobox.Input 
+                    displayValue={(item: string) => item}
+                    onChange={handleQuery}                 
+                    as={Input}
+                />     
+                <Transition
+                    as={Fragment}
+                    leave='transition ease-in duration-100'
+                    leaveFrom='opacity-100'
+                    leaveTo='opacity-0'
+                    afterLeave={initQuery}
+                >
+                    <Combobox.Options className='absoulte mt-1'>
+                        {
+                            (filteredManufactures.length === 0 && query !== '') ? (
+                                <Combobox.Option value={query}>
+                                    Create &quot;{query}&ldquo;
+                                </Combobox.Option>
+                            ) : filteredManufactures.map((item) => (
+                                <Combobox.Option key={item} className={({ active }) => `relative ${active ? 'bg-primary-blue text-white' : 'text-gray-900'}`} value={item}>
+                                    {
+                                        ({ selected, active }) => (
+                                            <>
+                                                <span className={`block truncate ${selected ? 'font-medium' : 'font-normal'}`}>
+                                                    {item}
+                                                </span>
+
+                                                {
+                                                    selected ? (
+                                                        <span className={`absolute inset-y-0 left-0 flex items-center pl-3 ${active ? 'text-white' : 'text-purple-500'}`} />
+                                                    ) : null
+                                                }
+                                            </>
+                                        )
+                                    }
+                                </Combobox.Option>
+                            ))
+                        }
+                    </Combobox.Options>
+                </Transition>        
+            </Combobox>
         </>
     );
 }
 
 export default SearchManufacturer;
 
+
+
+
+const Input = forwardRef((props, ref) => {
+    return (
+        <MuiInput className='w-full rounded-full py-0.5' ref={ref} {...props} placeholder='Volkswagen...' />
+    );
+});
+
+Input.displayName = 'Input';
